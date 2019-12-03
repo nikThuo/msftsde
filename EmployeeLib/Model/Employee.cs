b9e59ff025aa376e2600e09f0b7aec3f317a1cd5
Thuo Njoroge <thuo.njoroge@technobraingroup.com>
2019-12-03 12:35:40 +0300

    Employee Hierachy

---
+++ EmployeeLib/Model/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TinyCsvParser.Mapping;
+
+namespace EmployeeLib.Model
+{
+  public class Employee
+    {
+        public string Id { get; set; }
+        public string ManagerId { get; set; }
+        public int Salary { get; set; }
+
+    
+    }
+
+    
+}
